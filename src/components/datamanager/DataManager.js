f05e2dacbfb5376c98e436acae4d4d139ef2f84c
Sullivan Pierce <sullypierce@gmail.com>
2022-02-05 17:40:42 -0500

    created datamanager component with fetch object that has methods for all fetch requests

---
+++ src/components/datamanager/DataManager.js
@@ -0,0 +1,50 @@
+
+
+const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Token ${localStorage.getItem("tw_token")}`
+}
+
+export const fetch =  {
+    get: (endpoint) => {
+        return fetch(`http://localhost:8000/${endpoint}`, {
+            headers:headers
+        })
+            .then(response => response.json())
+    },
+
+    getOne: (endpoint, id) => {
+        return fetch(`http://localhost:8000/${endpoint}/${id}`, {
+            headers:headers
+        })
+            .then(response => response.json())
+    },
+
+    post: (endpoint, object) => {
+        return fetch(`http://localhost:8000/${endpoint}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Token ${localStorage.getItem("tw_token")}`
+            },
+            body: JSON.stringify(object)
+         })
+    },
+
+    update: (endpoint, id, object) => {
+        return fetch(`http://localhost:8000/${endpoint}/${id}`, {
+            method: "PUT",
+            headers: headers,
+            body: JSON.stringify(object)
+         })
+    },
+
+    delete: (endpoint, id) => {
+        return fetch(`http://localhost:8000/${endpoint}/${id}`, {
+            method: 'DELETE',
+            headers:headers
+        })
+            .then(response => response.json())
+    }
+
+}
