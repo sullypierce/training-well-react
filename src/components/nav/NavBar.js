209d29db70426089e591919add490fb6e5066e4f
Sullivan Pierce <sullypierce@gmail.com>
2022-02-09 11:02:39 -0500

    a little bit of css on the navbar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -16,26 +16,25 @@ export const NavBar = () => {
             <Link className="nav-link" to="/exercises">Exercises</Link>
             </li>
             <li className="navbar__item">
-            <Link className="nav-link" to="/benchmarks">Benchmarks</Link>
+            <Link className="nav-link" to="/benchmarks">My Benchmarks</Link>
             </li>
             <li className="navbar__item">
             <Link className="nav-link" to="/profile">My Profile</Link>
             </li>
             {
                 (localStorage.getItem("tw_token") !== null) ?
-                    <li className="nav-item">
-                        <button className="nav-link fakeLink"
-                            onClick={() => {
+                <li className="navbar__item">
+                            <Link className="nav-link" to='/login' onClick={() => {
                                 localStorage.removeItem("tw_token")
-                                navigate('/')
-                            }}
-                        >Logout</button>
-                    </li> :
+                            }}>Logout</Link>
+                        </li>
+                            
+                         :
                     <>
-                        <li className="nav-item">
+                        <li className="navbar__item">
                             <Link className="nav-link" to="/login">Login</Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="navbar__item">
                             <Link className="nav-link" to="/register">Register</Link>
                         </li>
                     </>
