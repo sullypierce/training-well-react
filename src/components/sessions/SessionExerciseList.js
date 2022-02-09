d34051837fec878d716792c236a5e0d2f7b8219b
Sullivan Pierce <sullypierce@gmail.com>
2022-02-09 14:42:02 -0500

    when a single session detail list is rendered, copies of all exercise logs are put in state, which get update when you change the input fields

--- src/components/sessions/SessionExerciseList.js
+++ src/components/sessions/SessionExerciseList.js
@@ -1,16 +1,39 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { TrainingPlanContext } from "../trainingplan/TrainingPlanDataProvider"
 
 export const SessionExerciseList = (props) => {
     const { singleViewSession, singleSessionExercises }  = useContext(TrainingPlanContext)
     
+    //hold the time completed if entered 
+    const [sessionTimeCompleted, setSessionTimeCompleted] = useState('');
+
+    //hold the exercises so they can be edited if the user chooses to log this session
+    const [thisSessionExercises, setThisSessionExercises] = useState([]);
+
+    useEffect(() => {
+        const exerciseArray = singleSessionExercises
+        setThisSessionExercises(exerciseArray)
+    }, [singleSessionExercises])
+
+    const changeState = (event) => {
+        const exercisesCopy = [...thisSessionExercises]
+        const elementName = event.target.name.split('_')[0]
+        const exerciseId = event.target.name.split('_')[1]
+        const newState = exercisesCopy.map((exercise) => {
+            if (exercise.id == exerciseId){
+                exercise[elementName] = event.target.value
+                return exercise
+            }
+        })
+        setThisSessionExercises(newState)
+    }
 
     return (
         <article className="sessions">
         <h2>Sessions</h2>
         <button className="btn btn-2 btn-sep icon-create"
         //onClick={() => sendToExerciseForm(0)}
-        >Add Exercise</button>
+        >Log Session</button>
             <h3>{singleViewSession.assigned_date}</h3>
             <h3>{singleViewSession.notes}</h3>
             {
@@ -18,10 +41,13 @@ export const SessionExerciseList = (props) => {
                     return <section key={`singleSessionExercise--${singleSessionExercise.id}`} className="singleSessionExercise">
 
                         <div className="singleSessionExercise__date">Exercise: {singleSessionExercise.exercise.name}</div>
-                        <div className="singleSessionExercise__notes">notes: {singleSessionExercise.notes}</div>
+                        <label htmlFor={`completed_${singleSessionExercise.id}`}>Notes:</label>
+                        <input type={'text'} name={`notes_${singleSessionExercise.id}`} className="singleSessionExercise__notes" value={singleSessionExercise.notes} onChange={changeState}/>
                         <div className="singleSessionExercise__notes">sets: {singleSessionExercise.sets}</div>
                         <div className="singleSessionExercise__notes">reps: {singleSessionExercise.reps}</div>
                         <div className="singleSessionExercise__notes">weight: {singleSessionExercise.weight_used}</div>
+                        <label htmlFor={`completed_${singleSessionExercise.id}`}>Completed</label>
+                        <input name={`completed_${singleSessionExercise.id}`} type='checkbox' value={singleSessionExercise.completed} onChange={changeState}/>
 
                         {/* <button className="btn btn-3"
                                     onClick={() => {sendTosingleSessionExerciseForm(singleSessionExercise.id)}}
