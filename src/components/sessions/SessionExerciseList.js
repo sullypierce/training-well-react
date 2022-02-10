354189089faff000cbf2f64c3a7280f665bd4ee2
Sullivan Pierce <sullypierce@gmail.com>
2022-02-10 16:59:26 -0500

    added fields for sleep hours and starting energy level, tested that they update state when changed

--- src/components/sessions/SessionExerciseList.js
+++ src/components/sessions/SessionExerciseList.js
@@ -5,7 +5,7 @@ export const SessionExerciseList = (props) => {
     const { singleViewSession, singleSessionExercises }  = useContext(TrainingPlanContext)
     
     //hold the time completed if entered 
-    const [sessionTimeCompleted, setSessionTimeCompleted] = useState('');
+    const [sessionCompleted, setSessionCompleted] = useState({...singleViewSession});
 
     //hold the exercises so they can be edited if the user chooses to log this session
     const [thisSessionExercises, setThisSessionExercises] = useState([]);
@@ -13,16 +13,20 @@ export const SessionExerciseList = (props) => {
     useEffect(() => {
         const exerciseArray = singleSessionExercises
         setThisSessionExercises(exerciseArray)
-        setSessionTimeCompleted(singleViewSession.time_completed)
     }, [singleSessionExercises])
 
     const changeState = (event) => {
-        if (event.target.name == 'session_time') {
-            setSessionTimeCompleted(event.target.value)
+        const elementName = event.target.name.split('__')[0]
+        //if element changed is part of session
+        if (elementName == 'session') {
+            const editSession = {...sessionCompleted}
+            editSession[event.target.name.split('__')[1]] = event.target.value
+            setSessionCompleted(editSession)
         } else {
+        //if element changed corresponds to an exercise
         const exercisesCopy = [...thisSessionExercises]
-        const elementName = event.target.name.split('_')[0]
-        const exerciseId = event.target.name.split('_')[1]
+        const exerciseId = event.target.name.split('__')[1]
+        //cycle through the exercises and change the state of the correct one
         const newState = exercisesCopy.map((exercise) => {
             if (exercise.id == exerciseId){
                 if (elementName=='completed') {
@@ -36,6 +40,8 @@ export const SessionExerciseList = (props) => {
         setThisSessionExercises(newState)
     }}
 
+    const numArray = [1,2,3,4,5,6,7,8,9,10]
+
     return (
         <article className="sessions">
         <h2>Sessions</h2>
@@ -45,8 +51,22 @@ export const SessionExerciseList = (props) => {
         <div className="session">
         <h3>{singleViewSession.assigned_date}</h3>
             <h3>{singleViewSession.notes}</h3>
-            <label htmlFor={`session_time`}>Time Completed:</label>
-            <input name="session_time" type={'time'} onChange={changeState} value={sessionTimeCompleted}/>
+            <label htmlFor={`session__time_completed`}>Time Completed:</label>
+            <input name="session__time_completed" type={'time'} onChange={changeState} value={sessionCompleted.time_completed}/>
+            <label htmlFor={`session__sleep_hours`}>Hours of Sleep last Night:</label>
+            <select name="session__sleep_hours"  onChange={changeState} value={sessionCompleted.sleep_hours}>
+                {
+                    
+                    numArray.map(num => <option key={num}>{num}</option>)
+                }
+            </select>
+            <label htmlFor={`session__energy_level`}>Starting Energy Level (1-10):</label>
+            <select name="session__energy_level"  onChange={changeState} value={sessionCompleted.energy_level}>
+                {
+                    
+                    numArray.map(num => <option key={num}>{num}</option>)
+                }
+            </select>
         </div>
             
             
@@ -55,13 +75,13 @@ export const SessionExerciseList = (props) => {
                     return <section key={`singleSessionExercise--${singleSessionExercise.id}`} className="singleSessionExercise">
 
                         <div className="singleSessionExercise__date">Exercise: {singleSessionExercise.exercise.name}</div>
-                        <label htmlFor={`completed_${singleSessionExercise.id}`}>Notes:</label>
-                        <input type={'text'} name={`notes_${singleSessionExercise.id}`} className="singleSessionExercise__notes" value={singleSessionExercise.notes} onChange={changeState}/>
+                        <label htmlFor={`notes__${singleSessionExercise.id}`}>Notes:</label>
+                        <input type={'text'} name={`notes__${singleSessionExercise.id}`} className="singleSessionExercise__notes" value={singleSessionExercise.notes} onChange={changeState}/>
                         <div className="singleSessionExercise__sets">sets: {singleSessionExercise.sets}</div>
                         <div className="singleSessionExercise__reps">reps: {singleSessionExercise.reps}</div>
                         <div className="singleSessionExercise__weight">weight: {singleSessionExercise.weight_used}</div>
-                        <label htmlFor={`completed_${singleSessionExercise.id}`}>Completed</label>
-                        <input name={`completed_${singleSessionExercise.id}`} type='checkbox' defaultValue={singleSessionExercise.completed} onChange={changeState}/>
+                        <label htmlFor={`completed__${singleSessionExercise.id}`}>Completed</label>
+                        <input name={`completed__${singleSessionExercise.id}`} type='checkbox' value={singleSessionExercise.completed} onChange={changeState}/>
 
                         {/* <button className="btn btn-3"
                                     onClick={() => {sendTosingleSessionExerciseForm(singleSessionExercise.id)}}
