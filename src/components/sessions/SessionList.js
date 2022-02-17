d4db92594c8b068f368cd64a19695fa59c73007c
Sullivan Pierce <sullypierce@gmail.com>
2022-02-17 11:44:17 -0500

    session list adds ref to nxt scheduled sessions div and auto srolls to it on render

--- src/components/sessions/SessionList.js
+++ src/components/sessions/SessionList.js
@@ -40,28 +40,12 @@ export const SessionList = (props) => {
         console.log(today)
         console.log(month, day, year)
         
-        for (let session of sessions) {
-            const dateArray = session.assigned_date.split('-')
-            console.log(dateArray)
-            const isNextSession = () => {
-                if (dateArray[0] >= year && parseInt(dateArray[1]) > month) {
-                    return true
-                } else if (dateArray[0] >= year && parseInt(dateArray[1]) == month && parseInt(dateArray[2]) >= day ) {
-                    return true
-                } else { return false}
-            }
-            if (isNextSession()){
-                console.log('setting today session')
-                setNextSession(session)
-                break
-            }
-        }
+        
     }, [])
 
     useEffect(() => {
         if(nextSessionRef.current){
-
-           // nextSessionRef.current.scrollIntoView({behavior: "smooth"})
+            nextSessionRef.current.scrollIntoView({behavior: "smooth"})
         }
     }, [nextSessionRef])
 
@@ -73,7 +57,7 @@ export const SessionList = (props) => {
         >Schedule New Session</button>
             {
                 sessions.map(session => {
-                    return <section id={`sesssion_${session.id}`} key={`session--${session.id}`} ref={nextSession.id == session.id ? nextSessionRef : null} className={session.time_completed != null ? 'card session complete': 'card session tbd'}>
+                    return <section id={`sesssion_${session.id}`} key={`session--${session.id}`} ref={session.next_scheduled ? nextSessionRef : null} className={session.time_completed != null ? 'card session complete': 'card session tbd'}>
 
                         <div className="session__date">Date: {session.assigned_date}</div>
                         <div className="session__notes">notes: {session.notes}</div>
